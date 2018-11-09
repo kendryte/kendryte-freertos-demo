915d94a1f3e68e674a2594a085f01509b857870f
jiangxiangbing <jiangxiangbing@canaan-creative.com>
2018-11-09 11:55:25 +0800

    add lcd demo of other boards

---
+++ lcd_lichee_pi/README.md
@@ -0,0 +1,6 @@
+LCD test
+=====
+
+1.Test lcd clear.<br/>
+2.Test lcd draw picture.<br/>
+3.Test lcd draw string.<br/>
---
+++ lcd_lichee_pi/main.c
@@ -0,0 +1,37 @@
+/* Copyright 2018 Canaan Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <stdio.h>
+#include <string.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include "lcd.h"
+#include "img.h"
+
+uint32_t lcd_gram[LCD_X_MAX * LCD_Y_MAX / 2] __attribute__((aligned(128)));
+
+int main(void)
+{
+    printf("lcd test\n");
+    lcd_init();
+    memset(lcd_gram, 0xc0, LCD_X_MAX * LCD_Y_MAX * 2);
+    lcd_clear(GREEN);
+    lcd_draw_picture(100, 0, 120, 240, lcd_gram);
+    lcd_draw_string(100, 50, "canaan kendryte",  BLUE);
+    lcd_draw_string(100, 90, "hello AI world", NAVY);
+	lcd_draw_picture(0, 0, 320, 240, (uint32_t*)gImage_1);
+	lcd_draw_picture(0, 0, 320, 240, (uint32_t*)gImage_2);
+    while (1)
+        ;
+}
